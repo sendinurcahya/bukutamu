a5ad4b0a606f24697a387b360da3a6c7b626092b
azickri123 <force.sektor@gmail.com>
2020-09-20 00:53:47 +0700

    Login by role

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request) {
+        $input = $request->all();
+
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if(Auth()->attempt(array('email' => $input['email'], 'password' => $input['password']))) {
+            if(Auth()->user()->level == 'admin') {
+                return redirect('/admin');
+            } else if(Auth()->user()->level == 'kepsek') {
+                return redirect('/kepsek');
+            } else {
+                return redirect('/login');
+            }
+        } else {
+            return redirect()->route('login')->with('error', 'email atau Password salah!');
+        }
+    }
+}
